04d850adbc86db78cee40c8258d7cf19526be049
Dorcas Chepkoech <dorcaschepkoech717@gmail.com>
2026-01-13 21:06:45 +0300

    patch

--- server/app.py
+++ server/app.py
@@ -23,12 +23,23 @@ def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
     return make_response(  bakeries,   200  )
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET', 'PATCH'])
 def bakery_by_id(id):
-
     bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
-    return make_response ( bakery_serialized, 200  )
+    
+    if request.method == 'GET':
+        bakery_serialized = bakery.to_dict()
+        return make_response(bakery_serialized, 200)
+    
+    elif request.method == 'PATCH':
+        for attr in request.form:
+            setattr(bakery, attr, request.form[attr])
+        
+        db.session.add(bakery)
+        db.session.commit()
+        
+        bakery_serialized = bakery.to_dict()
+        return make_response(bakery_serialized, 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
@@ -44,6 +55,34 @@ def most_expensive_baked_good():
     most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
     most_expensive_serialized = most_expensive.to_dict()
     return make_response( most_expensive_serialized,   200  )
+
+@app.route('/baked_goods', methods=['POST'])
+def baked_goods():
+    new_baked_good = BakedGood(
+        name=request.form.get('name'),
+        price=request.form.get('price'),
+        bakery_id=request.form.get('bakery_id')
+    )
+    
+    db.session.add(new_baked_good)
+    db.session.commit()
+    
+    baked_good_serialized = new_baked_good.to_dict()
+    return make_response(baked_good_serialized, 201)
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def baked_good_by_id(id):
+    baked_good = BakedGood.query.filter_by(id=id).first()
+    
+    db.session.delete(baked_good)
+    db.session.commit()
+    
+    response_body = {
+        "delete_successful": True,
+        "message": "Baked good deleted."
+    }
+    
+    return make_response(response_body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
